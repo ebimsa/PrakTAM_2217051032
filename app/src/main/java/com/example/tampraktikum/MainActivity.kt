5b8072a857ea43ee654cb226edc8881a5011d08b
Enggal... <enggalbimas8@gmail.com>
2026-03-05 19:51:45 +0700

    Revert "Modul 4, Tam"
    This reverts commit c6d83c8c16d2544d6277197a5e4c62530533de82.

--- app/src/main/java/com/example/tampraktikum/MainActivity.kt
+++ app/src/main/java/com/example/tampraktikum/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.tampraktikum
 
-import com.Food.model.Food
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,17 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tampraktikum.model.FoodSource
@@ -33,76 +27,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TAMPraktikumTheme {
-                DaftarMakananScreen()
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
 
 @Composable
-fun DaftarMakananScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(24.dp)
-    ){
-        FoodSource.dummyFood.forEach { food ->
-            DetailScreen (food = food)
-            Spacer(modifier = Modifier.height(24.dp))
-        }
-    }
-}
+fun Greeting(modifier: Modifier = Modifier) {
+    val food = FoodSource.dummyFood[0]
 
-@Composable
-fun DetailScreen(food: Food) {
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(modifier = modifier.fillMaxSize().padding(24.dp)) {
         Image(
             painter = painterResource(id = food.imageRes),
             contentDescription = food.nama,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp),
+                .height(200.dp)
+                .padding(bottom = 16.dp),
             contentScale = ContentScale.Crop
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = food.nama,
-            style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = food.deskripsi,
-            style = MaterialTheme.typography.bodyLarge
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = "Harga: Rp.${food.harga}",
-            style = MaterialTheme.typography.bodyLarge
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = { },
-            modifier = Modifier.fillMaxWidth()
-        ){
-            Text("Pesan Sekarang")
-        }
+        Text(text = "Name: ${food.nama}")
+        Text(text = "Description: ${food.deskripsi}")
+        Text(text = "Harga: ${food.harga}")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DaftarMakananScreenPreview() {
+fun GreetingPreview() {
     TAMPraktikumTheme {
-        DaftarMakananScreen()
+        Greeting()
     }
 }
